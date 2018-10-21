2b1be59615123dd3d28a79df86caed3a1b2cb0a8
nhut huynh <nhut.huynh@gmail.com>
2018-10-21 20:12:07 +1100

    create heroku config

---
+++ build/precache-manifest.94af8af7c606333a088f51a39ee55eab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9447b4796d1fc08d4919",
+    "url": "/static/js/main.9447b479.chunk.js"
+  },
+  {
+    "revision": "8ac0d35faace82c91e56",
+    "url": "/static/js/1.8ac0d35f.chunk.js"
+  },
+  {
+    "revision": "9447b4796d1fc08d4919",
+    "url": "/static/css/main.befaa3af.chunk.css"
+  },
+  {
+    "revision": "8ac0d35faace82c91e56",
+    "url": "/static/css/1.d8769437.chunk.css"
+  },
+  {
+    "revision": "05eceea10863fd006e3fb9734bbf3b66",
+    "url": "/index.html"
+  }
+];
