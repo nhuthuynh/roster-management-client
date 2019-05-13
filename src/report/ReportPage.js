806caa038bf0ba54fa7a548d161057a4f38cf943
nhut huynh <nhut.huynh@gmail.com>
2019-05-13 16:08:12 +1000

    remove select employee component out of report page

--- src/report/ReportPage.js
+++ src/report/ReportPage.js
@@ -1,39 +1,12 @@
 import React, { Component } from 'react'
-import { loadEmployees, loadLeaveRequestsInfoOfAnEmployee } from '../util/APIUtils'
-import { Select, Row, Col, notification } from 'antd'
-import { getShopOwnerId } from '../util/helper'
+import { loadLeaveRequestsInfoOfAnEmployee } from '../util/APIUtils'
+import { notification } from 'antd'
 import LeaveInfo from '../leave/LeaveInfo'
-const Option = Select.Option;
+import EmployeeSelection from '../common/EmployeesSelection';
 
 export default class EmployeesPage extends Component {
     state = {
-        employees: [],
         leaveInfo: {}
-    }
-
-    componentDidMount() {
-        this.loadEmployees()
-    }
-
-    renderEmployeesOption = (item, index) => {
-        return <Option key={index} value={item.id}>{`${item.firstName} ${item.lastName}`}</Option> 
-    }
-
-    renderSelectItem = (employees) => {
-        return employees && employees.length > 0 ? employees.map(this.renderEmployeesOption) : []
-    }
-
-    loadEmployees = () => {
-        const { currentUser } = this.props
-        
-        loadEmployees(getShopOwnerId(currentUser)).then((response) => {
-            this.setState((prevState) => {
-                return {
-                    ...prevState,
-                    employees: [{ id: 0, firstName: '', lastName: '' }].concat(response)
-                }
-            })
-        })
     }
 
     loadLeaveRequestsInfo = (id) => {
@@ -61,15 +34,13 @@ export default class EmployeesPage extends Component {
     }
 
     render () {
-        const { employees, leaveInfo } = this.state
+        const { currentUser } = this.props
+        const { leaveInfo } = this.state
         return (
             <div className="report-container">
                 <h1 className="page-title">report</h1>
                 <div className="page-body">
-                    <Row>
-                        <Col span="6">Select:</Col>
-                        <Col span="6"><Select onChange={this.onChangeEmployee} style={{ width: 160 }}>{this.renderSelectItem(employees)}</Select></Col>
-                    </Row>
+                    <EmployeeSelection onChangeEmployee={this.onChangeEmployee} currentUser={currentUser}/>
                     <LeaveInfo data={leaveInfo} />
                 </div>
             </div>
