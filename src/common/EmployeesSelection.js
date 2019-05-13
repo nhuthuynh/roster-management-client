233ad4195e89e55075b2bdf52cb15d6c5ad78387
nhut huynh <nhut.huynh@gmail.com>
2019-05-13 16:07:37 +1000

    seperate Select Employee Component from ReportPage

---
+++ src/common/EmployeesSelection.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { loadWorkingEmployees } from '../util/APIUtils'
+import { Select, Row, Col } from 'antd'
+import { getShopOwnerId } from '../util/helper'
+
+const Option = Select.Option;
+
+export default class EmployeeSelection extends Component {
+
+    state = {
+        employees: []
+    }
+
+    componentDidMount () {
+        this.loadEmployees()
+    }
+
+    loadEmployees = () => {
+        const { currentUser } = this.props
+        
+        loadWorkingEmployees(getShopOwnerId(currentUser)).then((response) => {
+            this.setState((prevState) => {
+                return {
+                    ...prevState,
+                    employees: [{ id: 0, firstName: '', lastName: '' }].concat(response)
+                }
+            })
+        })
+    }
+
+    renderEmployeesOption = (item, index) => {
+        return <Option key={index} value={item.id}>{`${item.firstName} ${item.lastName}`}</Option> 
+    }
+
+    renderSelectItem = (employees) => {
+        return employees && employees.length > 0 ? employees.map(this.renderEmployeesOption) : []
+    }
+
+    render () {
+        const { renderSelectItem } = this
+        const { onChangeEmployee, data } = this.props
+        const { employees } = this.state
+
+        return (
+            <Row>
+                <Col span="6">Select:</Col>
+                <Col span="6"><Select onChange={onChangeEmployee} style={{ width: 160 }}>{renderSelectItem(employees)}</Select></Col>
+            </Row>
+        )
+    }
+}
