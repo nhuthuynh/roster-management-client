16c7a056fbf22c55865fd2133f343a14f74bc78a
nhut huynh <nhut.huynh@gmail.com>
2019-06-04 12:19:59 +1000

    separate components for reuse

---
+++ src/availability/AvailabilityGroup.js
@@ -0,0 +1,59 @@
+import React, { PureComponent } from 'react'
+import AvailabilityDay from './AvailabilityDay'
+import { Button } from 'antd'
+import { formatNumberWithLeadingZero } from '../util/helper'
+
+export default class AvailabilityGroup extends PureComponent {
+
+    mapEachAvalabilityDay = (el, index) => {
+
+        const { mode, onInputChange, onInputBlur, onChangeAvailability } = this.props
+        const { available, day, startHour, startMinute, endHour, endMinute } = el
+        let startTime = "", endTime= ""
+        if (!available && mode === "view") {
+            startTime = "N/A"
+            endTime = "N/A"
+        } else {
+            startTime = formatNumberWithLeadingZero(startHour, startMinute)
+            endTime = formatNumberWithLeadingZero(endHour, endMinute)
+        }
+
+        return <AvailabilityDay key={index} startTime={startTime} endTime={endTime} day={day} index={index} 
+            startTimeTooltipValue={"Enter start time in HH:mm format"}
+            endTimeTooltipValue={"Enter end time in HH:mm format"}
+            mode={mode}
+            available={available}
+            onCheckBoxChange={onChangeAvailability} 
+            onInputChange={onInputChange} 
+            onInputBlur={onInputBlur}
+             />
+    }
+
+    renderAvailableDays = (availabilities) => {
+        if (!availabilities) return null
+        
+        return availabilities.map(this.mapEachAvalabilityDay) 
+        
+    }
+
+    render () {
+        const { mode, data, onCancelEditMode, onSave, onSwitchEditMode } = this.props
+        const { renderAvailableDays } = this
+        return (
+            <div className="availability-group">
+            {
+                renderAvailableDays(data)
+            }
+            {
+                mode === "view" ? <Button className="btn-first" onClick={onSwitchEditMode} type="primary">Edit</Button> : null
+            }
+            {
+                mode !== "view" ? <Button onClick={onCancelEditMode}>Cancel</Button> : null
+            }
+            {
+                mode !== "view" ? <Button className="btn-first" onClick={onSave} type="primary">Save</Button> : null
+            }
+            </div>
+        )
+    }
+}
---
+++ src/availability/AvailabilityNotification.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AvailabilityNotification(props) {
+    const { data: roster } = props
+    return (
+        <p>
+            {
+                roster && roster.id ? <b>{ `A roster from ${roster.fromDate} to ${roster.toDate} is made. Your update will affected after ${roster.toDate}` }</b> : null
+            }
+        </p>
+    )
+}
